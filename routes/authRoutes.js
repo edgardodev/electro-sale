3529e2c9b0f5454cf88fc60e4af6a97927f6ae2f
edgardodev <edgardoflorez1@gmail.com>
2025-09-10 22:43:34 -0500

    Mejorado el diseño del chat y estilos responsive

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router();   
 const authController = require("../controllers/authController");
+
+// Ruta de prueba
+router.get("/test", (req, res) => {
+  res.send("Ruta de auth funcionando ✅");
+});
 
 // Ruta para registrar usuario nuevo
 router.post("/register", authController.registerUser);
